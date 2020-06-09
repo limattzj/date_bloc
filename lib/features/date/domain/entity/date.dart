383d8b85fffc70a5857d2cc949468194f5b8b74b
limattzj <limattzj@gmail.com>
2020-06-09 12:14:57 -0700

    refactoring all the tests

--- lib/features/date/domain/entity/date.dart
+++ lib/features/date/domain/entity/date.dart
@@ -1,13 +1,12 @@
 import 'package:equatable/equatable.dart';
+import 'dart:math';
 
 class Date extends Equatable {
   final String message;
   final DateTime targetDate;
   const Date({this.message, this.targetDate});
 
-  /// Return numbers of days left from [tomorrow 00:00] to [targetDate 00:00].
-  /// This getter cannot be test because it finds a difference between
-  /// [targetDate] and [now], and [now] is always changing.
+  /// return the number of days between [now] and [targetDate]
   int get daysDifference {
     return getDaysDifference(
       DateTime.now(),
@@ -15,20 +14,23 @@ class Date extends Equatable {
     );
   }
 
-  int getDaysDifference(DateTime startDate, DateTime targetDate) {
+  /// return the number of days between x & y dates
+  int getDaysDifference(DateTime x, DateTime y) {
     // tomorrow at 00:00
-    final nextDay = DateTime.utc(
-      startDate.year,
-      startDate.month,
-      startDate.day + 1,
-    );
-
-    // if targetDate is in the future, result += 1
-    return targetDate.isBefore(DateTime.now())
-        ? targetDate.difference(nextDay).inDays
-        : targetDate.difference(nextDay).inDays + 1;
+    return x.difference(y).inDays.abs();
   }
 
   @override
   List<Object> get props => [message, targetDate];
 }
+
+//  final nextDay = DateTime.utc(
+//       x.year,
+//       x.month,
+//       x.day + 1,
+//     );
+
+//     // if targetDate is in the future, result += 1
+//     return y.isBefore(DateTime.now())
+//         ? y.difference(nextDay).inDays
+//         : y.difference(nextDay).inDays + 1;
